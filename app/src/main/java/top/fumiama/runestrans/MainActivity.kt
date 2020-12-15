6611fabdd79931f23ca11ee7542abe882abc26cd
fumiama <s862105088@gmail.com>
2020-12-15 18:16:51 +0800

    v1.5

--- app/src/main/java/top/fumiama/runestrans/MainActivity.kt
+++ app/src/main/java/top/fumiama/runestrans/MainActivity.kt
@@ -3,21 +3,28 @@ package top.fumiama.runestrans
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.view.children
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.endrnd.*
 import kotlinx.android.synthetic.main.endrnd.view.*
 import kotlinx.android.synthetic.main.startrnd.view.*
 
+
 class MainActivity : Activity() {
+    var firstItem = true
     @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListenerToRootView()
         yaru.setOnClickListener {
             if(inptxt.text.isNotEmpty()){
                 val chat = layoutInflater.inflate(R.layout.startrnd, ln, false)
@@ -25,24 +32,43 @@ class MainActivity : Activity() {
                 chat.tl.text = turn(inptxt.text.toString())
                 rchat.tr.text = inptxt.text
                 inptxt.text.clear()
-                ln.addView(rchat, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT))
+                if(ln.childCount > 0) firstItem = false
+                ln.addView(
+                    rchat, LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                )
                 ln.addView(TextView(this))
-                ln.addView(chat, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT))
+                ln.addView(
+                    chat, LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                    )
+                )
                 ln.addView(TextView(this))
                 setColor(chat.cl)
                 setColor(rchat.cr)
                 scroll.post { scroll.fullScroll(ScrollView.FOCUS_DOWN); inptxt.requestFocus(); }
+                if(!firstItem && scroll.translationY == 0f) scroll.upOverScroll()
             }
         }
         yaru.setOnLongClickListener {
+            firstItem = true
             for(i in ln.children.iterator()){
-                ObjectAnimator.ofFloat(i, "alpha", 1F, 0F).setDuration(111 + ln.indexOfChild(i).toLong() * 10).start()
-                ObjectAnimator.ofFloat(i, "translationX", i.translationX, if(ln.indexOfChild(i) % 4 != 0) -i.width.toFloat() else i.width.toFloat()).setDuration(111 + ln.indexOfChild(i).toLong() * 10).start()
+                ObjectAnimator.ofFloat(i, "alpha", 1F, 0F).setDuration(
+                    111 + ln.indexOfChild(i).toLong() * 10
+                ).start()
+                ObjectAnimator.ofFloat(
+                    i,
+                    "translationX",
+                    i.translationX,
+                    if (ln.indexOfChild(i) % 4 != 0) -i.width.toFloat() else i.width.toFloat()
+                ).setDuration(111 + ln.indexOfChild(i).toLong() * 10).start()
             }
-            ln.postDelayed({ln.removeAllViews()}, 111 + ln.childCount.toLong() * 10)
+            ln.postDelayed({ ln.removeAllViews() }, 111 + ln.childCount.toLong() * 10)
             true
         }
-        inptxt.setOnClickListener { scroll.postDelayed({scroll.fullScroll(ScrollView.FOCUS_DOWN); inptxt.requestFocus();}, 233) }
     }
     private fun setColor(v: CardView){
         when((0..5).random()){
@@ -52,6 +78,30 @@ class MainActivity : Activity() {
             3 -> v.setCardBackgroundColor(resources.getColor(R.color.colord, theme))
             4 -> v.setCardBackgroundColor(resources.getColor(R.color.colore, theme))
             5 -> v.setCardBackgroundColor(resources.getColor(R.color.colorf, theme))
+        }
+    }
+    private fun setListenerToRootView() {
+        val rootView: View = window.decorView.rootView
+        val softKeyboardHeight = 100
+        var heightDiff = 0
+        rootView.viewTreeObserver.addOnGlobalLayoutListener {
+            val r = Rect()
+            rootView.getWindowVisibleDisplayFrame(r)
+            val dm: DisplayMetrics = rootView.resources.displayMetrics
+            val hd: Int = rootView.bottom - r.bottom
+            if(hd != heightDiff){
+                heightDiff = hd
+                val isKeyboardShown = heightDiff > softKeyboardHeight * dm.density
+                if (isKeyboardShown) {
+                    //Toast.makeText(this, "弹出$heightDiff", Toast.LENGTH_SHORT).show()
+                    if(!firstItem || scroll.getChildAt(0).height > rootView.height) scroll.upOverScroll()
+                    inptxt.requestFocus()
+                } else {
+                    //Toast.makeText(this, "收回$heightDiff", Toast.LENGTH_SHORT).show()
+                    if(!firstItem || scroll.getChildAt(0).height > rootView.height) scroll.downToNormal()
+                }
+            }
+            if(scroll.maxOverScrollY == 0) scroll.maxOverScrollY = yaru.height / 64 * 150
         }
     }
 
